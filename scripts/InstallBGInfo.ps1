5b1ec1a6167a1b0f6ff8953963d43bce8dc4afe3
Paul Broadwith <paul@pauby.com>
2018-10-03 16:56:24 +0100

    (maint) Remove specific local package source

--- scripts/InstallBGInfo.ps1
+++ scripts/InstallBGInfo.ps1
@@ -2,7 +2,7 @@ if ($null -eq (Get-Command -Name 'choco.exe' -ErrorAction SilentlyContinue)) {
     Write-Warning "Chocolatey not installed. Cannot install BGInfo."
 }
 else {
-    choco upgrade bginfo -y --source="'c:\packages'"
+    choco upgrade bginfo -y
 
     #Copy-Item "C:\vagrant\shell\bginfo.bgi" 'C:\programdata\chocolatey\lib\sysinternals\tools\bginfo.bgi'
     Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name bginfo -Value 'c:\programdata\chocolatey\lib\bginfo\tools\bginfo.exe c:\vagrant\shell\bginfo.bgi /accepteula /silent /timer:0'

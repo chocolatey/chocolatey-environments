4b093495f603d2413c6702e9221b1df6f8ae4bf3
Paul Broadwith <paul@pauby.com>
2019-04-15 14:24:27 +0100

    (maint) Use script to install package

--- scripts/InstallBGInfo.ps1
+++ scripts/InstallBGInfo.ps1
@@ -1,9 +1,4 @@
-if ($null -eq (Get-Command -Name 'choco.exe' -ErrorAction SilentlyContinue)) {
-    Write-Warning "Chocolatey not installed. Cannot install BGInfo."
-}
-else {
-    choco upgrade bginfo -y
+& \scripts\InstallChocoPackage.ps1 -Name bginfo -Version 4.25
 
-    #Copy-Item "C:\vagrant\shell\bginfo.bgi" 'C:\programdata\chocolatey\lib\sysinternals\tools\bginfo.bgi'
-    Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name bginfo -Value 'c:\programdata\chocolatey\lib\bginfo\tools\bginfo.exe c:\vagrant\shell\bginfo.bgi /accepteula /silent /timer:0'
-}
+Copy-Item "\scripts\bginfo.bgi" 'C:\programdata\chocolatey\lib\bginfo\tools\bginfo.bgi'
+Set-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run -Name bginfo -Value 'c:\programdata\chocolatey\lib\bginfo\tools\bginfo.exe C:\programdata\chocolatey\lib\bginfo\tools\bginfo.bgi /accepteula /silent /timer:0'

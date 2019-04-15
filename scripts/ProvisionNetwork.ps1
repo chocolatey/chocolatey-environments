5f9c32384961c71352306b15735949f97fdc46e1
Paul Broadwith <paul@pauby.com>
2019-04-15 14:32:10 +0100

    (maint) Merged into PrepareWindows script

--- scripts/ProvisionNetwork.ps1
+++
@@ -1,7 +0,0 @@
-Get-NetAdapter | ForEach-Object {
-  Write-Host "Setting '$($_.Name)' interface to a 'Private' network."
-  Get-NetConnectionProfile -InterfaceIndex $_.ifIndex | Set-NetConnectionProfile -NetworkCategory Private
-}
-
-Write-Host "Disabling IPv6"
-Get-NetAdapterBinding | Where-Object ComponentID -eq 'ms_tcpip6' | Disable-NetAdapterBinding
